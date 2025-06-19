eabaa3b9f33c2ec87c4916c0114525c6750b4738
Arko2016 <arkojyoti.hore@gmail.com>
2025-06-19 16:39:58 -0700

    showed modularity feature of OOPs

---
+++ chatbotfile2.py
@@ -0,0 +1,7 @@
+#Brief example to show modularity through OOPs
+
+#import the user-defined class that we built in the chatbotfile1.py
+from chatbotfile1 import chatbot
+
+#since we have imported the class, we can now create object of the class in this file
+user1 = chatbot()
